07d81d4972626cc65524d1e9f5fd48112e90f871
Hsuan <yuhsuan050308@gmail.com>
2025-07-05 00:36:38 +0800

    Add files via upload

---
+++ 5vs5_new/0705_1235/code/Mylib.py
@@ -0,0 +1,12 @@
+import torch
+import numpy as np
+
+class Mylib:
+    def __init__(self, agent=None):
+        """
+        初始化 Mylib
+        :param agent: MyAgent 實例的引用
+        """
+        self.agent = agent
+
+
---
+++ 5vs5_new/0705_1235/code/init.lua
@@ -0,0 +1,20 @@
+-- Reference: https://github.com/rjstone/cmano-lua-helpers/blob/master/README.md
+-- Run this script in the in-game script console during the first load of the scenario to set up handlers for agent action and observation export
+-- Save after running script and you will not have to run this script again
+
+-- Edit these variables as needed
+local pycmo_path = 'C:/Users/yuhsu/OneDrive/桌面/pycmopen/'
+local cmo_path = 'C:/Program Files (x86)/Steam/steamapps/common/Command - Modern Operations/ImportExport'
+local lua_script_foldername = pycmo_path .. 'scripts/MyNet_episode_battle/'
+local pycmo_lib_lua_filename = pycmo_path .. 'lua/pycmo_lib.lua'
+local agent_action_lua_filename = cmo_path .. '/agent_action.lua'
+local export_observation_lua_filename = 'export_observation.lua'
+local execute_agent_action_every_seconds = '2' -- '0' = 1 second, '1' = 5 seconds, '2' = 15 seconds, '3' = 30 seconds, '4' = 1 minute, '5' = 5 minutes, '6' = 15 minutes, '7' = 30 minutes, '8' = 1 hour
+local export_observations_every_seconds = '2'
+local time_compression = 5 -- 0 = x1, 1 = x2, 2 = x5, 3 = x15, 4 = turbo, 5 = double turbo
+
+-- Call script to setup observation export and agent action handler
+ScenEdit_RunScript(pycmo_path .. 'lua/init.lua', true)
+setup_observation_export_agent_action(lua_script_foldername, pycmo_lib_lua_filename, agent_action_lua_filename, export_observation_lua_filename, execute_agent_action_every_seconds, export_observations_every_seconds, time_compression)
+
+ScenEdit_SetEMCON('Side','T','Radar=Active;Sonar=Active;OECM=Active')
